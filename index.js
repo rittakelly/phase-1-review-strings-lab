20ad5182ceb4fd695c8c8453f326873d27fb5188
Ritta <brittanykelly33@gmail.com>
2022-04-25 16:09:19 -0400

    commit tree 10b2e128d91fd1e103f3d59a43f3d6a00ae129cb

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Ritta";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()}!`;
+const firstInitial = "R" 
+const restOfName = currentUser.slice(1)
+const shortGreeting = `Welcome, ${firstInitial}!`;
